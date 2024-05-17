b41ab30613430df1c49a6eea28ff27a62f88fcbe
Gjss333 <gleydson_jose333@outlook.com>
2024-05-17 01:40:17 -0300

    feat(Icons): Implementation of two icons for the About Component

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import './assets/tailwind.css'
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faMedal } from '@fortawesome/free-solid-svg-icons';
+import { faUserGraduate } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faLinkedin, faGithub,);
+library.add(faLinkedin, faGithub, faMedal, faUserGraduate);
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
