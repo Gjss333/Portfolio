c63435982a836629d47ed659aeb6d903e0896ca1
Gjss333 <gleydson_jose333@outlook.com>
2024-06-05 17:47:14 -0300

    feat(Component): adding skills session

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,12 +1,12 @@
 <template>
     <section class="flex justify-center gap-52 items-center h-[96vh] mx-[10rem]">
-        <div class="tt">
+        <div>
             <h1 class="text-5xl text-center py-5 font-extrabold">Sobre mim</h1>
             <p class="w-[35rem] font-medium text-justify text-gray-500">Sou um apaixonado desenvolvedor web full stack com experiência em PHP, Laravel, Vue.js, React e Node.js. Minha paixão é criar interfaces cativantes e eficientes, combinando habilidades em Bootstrap e Tailwind para designs responsivos e interativos. Tenho experiência em desenvolvimento e manutenção de aplicações e APIs REST, utilizando práticas de TDD e versionamento de código com GIT. Além disso, mantenho-me atualizado com as últimas tecnologias, sempre buscando elevar o padrão do meu trabalho.</p>
             
             <div class="flex justify-center text-center gap-4 mt-6">
 
-                <div class="w-[50%] border-double rounded-3xl p-4  border-gray-500 border-2">
+                <div class="w-[50%] border-solid rounded-3xl p-4  border-gray-500 border-2">
                     <font-awesome-icon class="text-3xl" :icon="['fas', 'medal']" />
                     <p class="text-2xl font-bold">Experiência</p>
                     <p class="text-gray-600">1+ anos</p>
@@ -14,7 +14,7 @@
 
                 </div>
                 
-                <div class="w-[50%] border-double rounded-3xl p-4 border-gray-500 border-2">
+                <div class="w-[50%] border-solid rounded-3xl p-4 border-gray-500 border-2">
                     <font-awesome-icon class="text-3xl" :icon="['fas', 'user-graduate']" />
                     <p class="text-2xl font-bold">Formação</p>
                     <p class="text-gray-600">Gestão de TI</p>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,8 +1,42 @@
 <template>
-<p>habilidades</p>
+    <section class="flex justify-center items-center h-[96vh] flex-col mx-[10rem]">
+        <div>
+            <h1 class="text-5xl text-center py-5 font-extrabold">Habilidades</h1>
+        </div>
+        <div class="flex max-w-[60rem] flex-wrap gap-5">
+            <div 
+                class="flex flex-col justify-center items-center w-28  border-2 border-gray-300 p-4 rounded-2xl"
+                v-for="(tech, index) in technologys" :key="index"
+            >
+            <img :src="tech.src" alt=""  class="h-10 w-11 mb-2">
+                <p class="flex-end text-xl text-center italic">{{tech.name}}</p>
+            </div>
+            
+        </div>
+    </section>
 </template>
 <script>
 export default{
     name: 'Skills',
+    data(){
+        return{
+            technologys: [
+                { name: 'HTML', src: '/img/language-icon/html-icon.png'},
+                { name: 'CSS', src: '/img/language-icon/css-icon.png'},
+                { name: 'Javascript', src: '/img/language-icon/javascript-icon.png'},
+                { name: 'Vue.js', src: '/img/language-icon/vue-icon.png'},
+                { name: 'React.js', src: '/img/language-icon/react-icon.png'},
+                { name: 'Node.js', src: '/img/language-icon/node-icon.png'},
+                { name: 'PHP', src: '/img/language-icon/php-icon.png'},
+                { name: 'Laravel', src: '/img/language-icon/laravel-icon.png'},
+                { name: 'MySQL', src: '/img/language-icon/mysql-icon.png'},
+                { name: 'Bootstrap', src: '/img/language-icon/bootstrap-icon.png'},
+                { name: 'Tailwind', src: '/img/language-icon/tailwind-icon.png'},
+                { name: 'Git', src: '/img/language-icon/git-icon.png'},
+                { name: 'Figma', src: '/img/language-icon/figma-icon.png'},
+                { name: 'Azure', src: '/img/language-icon/azure-icon.png'},
+            ]
+        }
+    }
 }
 </script>
